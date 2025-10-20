ee1cff4c5f776c85bda4f4d4e5862eefb1c846cb
edalzell <erin@silentz.co>
2025-10-20 15:24:07 -0700

    tests

--- tests/Concerns/Features/HasDatabaseTest.php
+++ tests/Concerns/Features/HasDatabaseTest.php
@@ -1,11 +1,16 @@
 <?php
 
 use Edalzell\Features\Feature;
+use Edalzell\Features\FeatureServiceProvider;
 
-it('checks for migrations', function () {
+it('registers migrations', function () {
     $localDisk = tap(mockOnDemandDisk('Features/One'))->put('database/migrations/add_table.php', '');
+    $provider = mock(new class(mock()) extends FeatureServiceProvider {});
 
-    expect(new Feature('One'))
-        ->hasMigrations()->toBeTrue()
-        ->migrationsPath()->toBe($localDisk->path('database/migrations'));
+    $provider
+        ->shouldReceive('loadMigrations')
+        ->once()
+        ->with($localDisk->path('database/migrations'));
+
+    (new Feature('One', $provider))->registerDatabase();
 });
