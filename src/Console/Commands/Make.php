1a1d7846c686fb9bad85a02d1b79910f44639ebf
edalzell <erin@silentz.co>
2025-10-19 21:55:48 -0700

    create feature

---
+++ src/Console/Commands/Make.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Edalzell\Features\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
+
+class Make extends GeneratorCommand
+{
+    protected $description = 'Add a new feature to the application';
+
+    protected $name = 'make:feature';
+
+    protected $type = 'Feature';
+
+    public function __construct(Filesystem $files, private Composer $composer)
+    {
+        parent::__construct($files);
+
+        $composer->setWorkingPath(base_path());
+    }
+
+    public function handle(): void
+    {
+        parent::handle();
+
+        $this->addComposerScript();
+    }
+
+    protected function getPath($name)
+    {
+        return app_path("Features/{$this->getNameInput()}/src/ServiceProvider.php");
+    }
+
+    protected function getStub()
+    {
+        return __DIR__.'/../../../stubs/provider.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace."\Features\\{$this->getNameInput()}";
+    }
+
+    private function addComposerScript(): void
+    {
+        tap($this->composer)
+            ->modify(fn (array $content) => $this->addPreAutoloadDumpScript($content))
+            ->dumpAutoloads();
+    }
+
+    private function addPreAutoloadDumpScript(array $content): array
+    {
+        $content['scripts']['pre-autoload-dump'] = 'Edalzell\\Features\\Composer\\FeatureNamespaces::add';
+
+        return $content;
+    }
+}
