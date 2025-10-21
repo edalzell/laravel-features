a619acfabfa6358b02add1bdb5b77497f5d34cfe
edalzell <erin@silentz.co>
2025-10-20 17:23:06 -0700

    tests and pass tests

--- tests/FeatureServiceProviderTest.php
+++ tests/FeatureServiceProviderTest.php
@@ -1,5 +1,50 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use Edalzell\Features\Feature;
+use Edalzell\Features\FeatureServiceProvider;
+
+it('can get feature', function () {
+    $disk = mockOnDemandDisk('Features/laravel-features');
+
+    expect((new ServiceProvider(mock()))->feature())
+        ->toBeInstanceOf(Feature::class)
+        ->slug->toBe('laravel-features');
 });
+
+it('can load migrations', function () {
+    $disk = mockOnDemandDisk('Features/laravel-features');
+
+    $provider = mock(ServiceProvider::class, [mock()])->shouldAllowMockingProtectedMethods()->makePartial();
+    $provider
+        ->shouldReceive('loadMigrationsFrom')
+        ->once()
+        ->with('database/migrations');
+
+    $provider->loadMigrations('database/migrations');
+});
+
+it('can load routes', function () {
+    $disk = mockOnDemandDisk('Features/laravel-features');
+
+    $provider = mock(ServiceProvider::class, [mock()])->shouldAllowMockingProtectedMethods()->makePartial();
+    $provider
+        ->shouldReceive('loadRoutesFrom')
+        ->once()
+        ->with('routes');
+
+    $provider->loadRoutes('routes');
+});
+
+it('can load views', function () {
+    $disk = mockOnDemandDisk('Features/laravel-features');
+
+    $provider = mock(ServiceProvider::class, [mock()])->shouldAllowMockingProtectedMethods()->makePartial();
+    $provider
+        ->shouldReceive('loadViewsFrom')
+        ->once()
+        ->with('resources/views', 'laravel-features');
+
+    $provider->loadViews('resources/views', 'laravel-features');
+});
+
+class ServiceProvider extends FeatureServiceProvider {}
--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -1,15 +1,51 @@
 <?php
 
 use Edalzell\Features\Feature;
+use Edalzell\Features\FeatureServiceProvider;
 
-it('can check for file existence', function () {
-    mockOnDemandDisk('Features/One')->put('foo/test.txt', 'contents');
+beforeEach(function () {
+    $this->disk = mockOnDemandDisk('Features/TwoWords');
+    $this->provider = mock(new class(mock()) extends FeatureServiceProvider {});
+    $this->feature = new Feature('TwoWords', $this->provider);
+});
 
-    $feature = new Feature('One');
+it('gets path', function () {
+    expect($this->feature->path('foo'))->toBe($this->disk->path('foo'));
+});
 
-    expect($feature->exists('foo/test.txt'))->toBeTrue();
+it('checks for file existence', function () {
+    $this->disk->put('foo/test.txt', 'contents');
+
+    expect($this->feature->exists('foo/test.txt'))->toBeTrue();
 });
 
 it('sets slug', function () {
-    expect(new Feature('TwoWords'))->toHaveProperty('slug', 'two-words');
+    expect($this->feature)->toHaveProperty('slug', 'two-words');
+});
+
+it('boots config', function () {
+    $this->disk->put('config/two-words.php', '');
+
+    $feature = mock(Feature::class, ['TwoWords', $this->provider])->makePartial();
+
+    $feature->shouldReceive('bootConfig')->once()->andReturnSelf();
+
+    $feature->boot();
+});
+
+it('registers all options', function () {
+    $this->disk->put('config/two-words.php', '');
+    $this->disk->put('database/migrations/add_table.php', '');
+    $this->disk->put('resources/views/test.blade.php', '');
+    $this->disk->put('routes/web.php', '');
+
+    $feature = mock(Feature::class, ['TwoWords', $this->provider])->makePartial();
+
+    $feature
+        ->shouldReceive('registerConfig')->once()->andReturnSelf()
+        ->shouldReceive('registerDatabase')->once()->andReturnSelf()
+        ->shouldReceive('registerRoutes')->once()->andReturnSelf()
+        ->shouldReceive('registerViews')->once()->andReturnSelf();
+
+    $feature->register();
 });
