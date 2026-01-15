b44e89a312b1c728307cb935301f45c81c0c9d97
edalzell <erin@silentz.co>
2026-01-14 21:20:43 -0800

    more paths

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -4,24 +4,24 @@ use Edalzell\Features\ServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
 
 it('registers feature', function () {
-    $featuresDisk = tap(mockOnDemandDisk('Features'))->put('TwoWords/src/ServiceProvider.php', '');
+    $featuresDisk = tap(mockOnDemandDisk('features'))->put('TwoWords/src/ServiceProvider.php', '');
 
-    File::expects('exists')->with(app_path('Features'))->andReturns(true);
+    File::expects('exists')->with(base_path('features'))->andReturns(true);
 
     $app = mock(Application::class);
 
     $app
         ->shouldReceive('register')
         ->once()
-        ->with('App\\Features\\TwoWords\\ServiceProvider');
+        ->with('Features\\TwoWords\\ServiceProvider');
 
     (new ServiceProvider($app))->register();
 });
 
 it('doesnt register feature when no provider', function () {
-    $featuresDisk = tap(mockOnDemandDisk('Features'))->put('TwoWords/src/Foo.php', '');
+    $featuresDisk = tap(mockOnDemandDisk('features'))->put('TwoWords/src/Foo.php', '');
 
-    File::expects('exists')->with(app_path('Features'))->andReturns(true);
+    File::expects('exists')->with(base_path('features'))->andReturns(true);
 
     $app = mock(Application::class);
 
