cbe8bda2c5bc4d67db13329d5895596b7927206d
edalzell <erin@silentz.co>
2026-03-06 12:47:27 +0100

    load package features

--- src/Composer/FeatureNamespaces.php
+++ src/Composer/FeatureNamespaces.php
@@ -22,10 +22,8 @@ class FeatureNamespaces
 
     private static function autoloadFeatures(array &$autoload, array &$autoloadDev): void
     {
-        $featurePaths = static::featurePaths('features');
-
-        foreach ($featurePaths as $featurePath) {
-            $featureName = basename($featurePath);
+        foreach (static::featurePaths('features') as $path) {
+            $featureName = basename($path);
             $rootNamespace = "Features\\{$featureName}\\";
             $dbRootNamespace = $rootNamespace.'Database\\';
             $rootPath = "features/{$featureName}/src/";
@@ -44,10 +42,28 @@ class FeatureNamespaces
 
     private static function autoloadPackageFeatures(array &$autoload, array &$autoloadDev): void
     {
-        $packageFeaturePaths = static::featurePaths('vendor/*/*/features');
+        if (empty($featurePaths = static::featurePaths('vendor/*/*/features'))) {
+            return;
+        }
 
-        foreach ($packageFeaturePaths as $packageFeaturePath) {
-            //
+        $namespace = static::packageNamespace($featurePaths);
+
+        foreach ($featurePaths as $path) {
+            // this will have a trailing slash
+            $featureName = basename($path);
+            $rootNamespace = "{$namespace}Features\\{$featureName}\\";
+            $dbRootNamespace = $rootNamespace.'Database\\';
+            $rootPath = "features/{$featureName}/src/";
+
+            $autoload['psr-4'][$rootNamespace] = $rootPath;
+
+            $factoryPath = "features/{$featureName}/database/factories";
+            $seedersPath = "features/{$featureName}/database/seeders";
+
+            $autoload['psr-4'][$dbRootNamespace.'Factories\\'] = $factoryPath;
+            $autoload['psr-4'][$dbRootNamespace.'Seeders\\'] = $seedersPath;
+
+            $autoloadDev['psr-4'][$rootNamespace.'Tests\\'] = "features/{$featureName}/tests";
         }
     }
 
@@ -58,5 +74,19 @@ class FeatureNamespaces
         }
 
         return array_filter($paths, 'is_dir');
+    }
+
+    private static function packageNamespace(array $featurePaths): string
+    {
+        // grab the first one, pop off the last segment, that's the package path
+        $segments = explode(DIRECTORY_SEPARATOR, $featurePaths[0]);
+
+        // remove the `features` segment
+        array_pop($segments);
+        $path = implode(DIRECTORY_SEPARATOR, $segments).DIRECTORY_SEPARATOR.'composer.json';
+
+        $composer = json_decode(file_get_contents($path), true);
+
+        return array_key_first($composer['autoload']['psr-4']);
     }
 }
--- tests/Composer/FeatureNamespacesTest.php
+++ tests/Composer/FeatureNamespacesTest.php
@@ -13,7 +13,10 @@ it('adds feature classes to namespaces', function () {
     $package = new RootPackage('edalzell/my-features', '1.0', 'v1.1');
     $composer = tap(new Composer)->setPackage($package);
 
+    $composerJson = json_encode(['autoload' => ['psr-4' => ['Edalzell\\FeaturesTest\\' => 'src/']]]);
+
     when('is_dir')->justReturn(true);
+    when('file_get_contents')->justReturn($composerJson);
     $featuresDir = getcwd().'/features';
     Functions\expect('glob')->once()->with($featuresDir.'/*')->andReturn([$featuresDir.'/One'])
         ->andAlsoExpectIt()->once()->with(getcwd().'/vendor/*/*/features/*')->andReturn([]);
@@ -27,5 +30,28 @@ it('adds feature classes to namespaces', function () {
             'Features\\One\\Database\\Seeders\\' => 'features/One/database/seeders',
         ]])->getDevAutoload()->toBe(['psr-4' => [
             'Features\\One\\Tests\\' => 'features/One/tests',
+        ]]);
+});
+it('adds package feature classes to namespaces', function () {
+    $package = new RootPackage('edalzell/my-features', '1.0', 'v1.1');
+    $composer = tap(new Composer)->setPackage($package);
+
+    $composerJson = json_encode(['autoload' => ['psr-4' => ['Edalzell\\FeaturesTest\\' => 'src/']]]);
+
+    when('is_dir')->justReturn(true);
+    when('file_get_contents')->justReturn($composerJson);
+    $featuresDir = getcwd().'/features';
+    Functions\expect('glob')->once()->with($featuresDir.'/*')->andReturn([])
+        ->andAlsoExpectIt()->once()->with(getcwd().'/vendor/*/*/features/*')->andReturn([$featuresDir.'/vendor/edalzell/my-features/One']);
+
+    FeatureNamespaces::add(new Event('pre-autoload-dump', $composer, new NullIO));
+
+    expect($package)
+        ->getAutoload()->toBe(['psr-4' => [
+            'Edalzell\\FeaturesTest\\Features\\One\\' => 'features/One/src/',
+            'Edalzell\\FeaturesTest\\Features\\One\\Database\\Factories\\' => 'features/One/database/factories',
+            'Edalzell\\FeaturesTest\\Features\\One\\Database\\Seeders\\' => 'features/One/database/seeders',
+        ]])->getDevAutoload()->toBe(['psr-4' => [
+            'Edalzell\\FeaturesTest\\Features\\One\\Tests\\' => 'features/One/tests',
         ]]);
 });
