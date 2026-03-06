b81f57b0753eff0c2b002a3e056d38378e585d44
edalzell <erin@silentz.co>
2026-03-06 12:56:52 +0100

    arch test

--- src/FeatureServiceProvider.php
+++ src/FeatureServiceProvider.php
@@ -25,7 +25,6 @@ abstract class FeatureServiceProvider extends LaravelServiceProvider
 
         $this->name = $this->name();
 
-        ray($this, $app);
         $this->disk = Storage::build([
             'driver' => 'local',
             'root' => base_path('features/'.$this->name),
