f721c2b0926fea32a2f0f8ee10dc0af8abcdfa67
Erin Dalzell <erin@silentz.co>
2026-05-23 17:12:40 -0700

    Auto-register policies (#39)
    * wip
    
    * tidy
    
    * sort

--- tests/Isolated/FeatureServiceProviderTest.php
+++ tests/Isolated/FeatureServiceProviderTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 
 it('register listeners', function () {
     Event::fake();
@@ -15,6 +16,18 @@ it('register listeners', function () {
     $provider->bootListeners();
 
     Event::assertListening('the-event', Listener::class);
+});
+
+it('registers policies', function () {
+    mockOnDemandDisk('features/TwoWords')->put('src/Policies/PostPolicy.php', '');
+    $provider = mockServiceProvider();
+    $provider->shouldReceive('namespace')->andReturn('Features\TwoWords');
+
+    Gate::shouldReceive('policy')
+        ->once()
+        ->with('Features\TwoWords\Models\Post', 'Features\TwoWords\Policies\PostPolicy');
+
+    $provider->bootPolicies();
 });
 
 class Listener
